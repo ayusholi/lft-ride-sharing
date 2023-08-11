5e30ffcdbf61661ffc78b48b8b902977af5f9004
Ayush Oli <ayush.oli@fusemachines.com>
2023-08-11 23:45:14 +0545

    chore: minor refactor

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,10 +4,11 @@ import { AppService } from './app.service';
 import { PassportModule } from '@nestjs/passport';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { AuthService } from './auth/auth.service';
 
 @Module({
   imports: [PassportModule, AuthModule, UsersModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AuthService],
 })
 export class AppModule {}
